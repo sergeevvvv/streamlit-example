3e0c55693d1dce9284983e81d7cd5bae28275369
sergeevvvv <96374570+sergeevvvv@users.noreply.github.com>
2022-06-28 01:55:01 +0300

    first test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,19 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+st.header('Descartes: перевод математических формул, записанных словами в LaTeX')
+st.text('Данная сборка модели обучена на арифметику, включая корни, логарифмы, степени, а также пределы и интегралы из математического анализа, функции суммы и произведения. Модель обучена на примерах минимальной вложенности на русском языке на латинском и греческих алфавитах не включая цифры.')
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+text = st.text_input(label, value="два икс плюс три", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+def translate(string):
+    return '$x^2$'
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+if st.button('Показать формулу'):
+    ans = translate(text)
+    st.text('Формула:')
+    st.text(ans)
+    
+    st.text('Ее символьный вид:')
+    st.latex(r'$'ans'$')
 
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+st.text('Баги и идеи на github:')
