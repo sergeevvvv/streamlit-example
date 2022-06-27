3cc46ae15ff8197633f7c5f2402dbe5ced22a3f8
sergeevvvv <96374570+sergeevvvv@users.noreply.github.com>
2022-06-28 01:56:39 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,10 @@ import streamlit as st
 st.header('Descartes: перевод математических формул, записанных словами в LaTeX')
 st.text('Данная сборка модели обучена на арифметику, включая корни, логарифмы, степени, а также пределы и интегралы из математического анализа, функции суммы и произведения. Модель обучена на примерах минимальной вложенности на русском языке на латинском и греческих алфавитах не включая цифры.')
 
-text = st.text_input(label, value="два икс плюс три", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False)
+text = st.text_input(label, value="два икс плюс три")
 
 def translate(string):
-    return '$x^2$'
+    return 'x^2'
 
 if st.button('Показать формулу'):
     ans = translate(text)
@@ -14,6 +14,6 @@ if st.button('Показать формулу'):
     st.text(ans)
     
     st.text('Ее символьный вид:')
-    st.latex(r'$'ans'$')
+    st.latex(ans)
 
 st.text('Баги и идеи на github:')
